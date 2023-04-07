7c3048d1dadaf9ed5658806cacc6dc9bc13ee61b
Nicholas Burney <nburney@Nicholass-MacBook-Pro.local>
2023-04-07 12:58:51 -1000

    cards showing

--- src/components/CardComponent/index.js
+++ src/components/CardComponent/index.js
@@ -1,13 +1,15 @@
-function CardComponent(songUrl, imageUrl, songName, artist) {
-  return (
-    <a href={songUrl}>
-      <div className="Image">
-        <img src={`${imageUrl}`} />
+function CardComponent({ imageUrl, songName, artistName, linkUrl }){
+    return (
+      <div className="card-component">
+        <a href={linkUrl}>
+          <img src={imageUrl} alt={songName} />
+          <div>
+            <h2>{songName}</h2>
+            <p>{artistName}</p>
+          </div>
+        </a>
       </div>
-      <h2>{songName}</h2>
-     <h5>{artist}</h5>
-    </a>
-  );
-}
+    );
+  };
 
 export default CardComponent;
--- src/components/SearchComponent/index.js
+++ src/components/SearchComponent/index.js
@@ -17,15 +17,10 @@ console.log(results);
       <input className="search" placeholder='Twista' onChange={(e) => setSearchTerm(e.target.value)}/>
       <button onClick={() => searchSong(searchTerm)}>Search</button>
 
-      {/* <CardComponent songUrl={"https://www.google.com/"} imageUrl={"https://www.thesprucepets.com/thmb/hxWjs7evF2hP1Fb1c1HAvRi_Rw0=/2765x0/filters:no_upscale():strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg"} songName={"Cool Song"} artist={"Cool Ass Dog"}/> */}
-
-
-      {/* {results ? */}
-
 
       {results.length
         ? results.map((value, i) => {
-            return <h1>{results[i].collectionName}</h1>
+            return <CardComponent imageUrl={value.artworkUrl100} songName={value.collectionName} artistName={value.artistName} linkUrl={value.collectionViewUrl}/>
         })
         : <h1>No songs yet!</h1>
       }
