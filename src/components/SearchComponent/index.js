a2fe24e66714ff726769b4de885e670de01c625c
Nicholas Burney <nburney@Nicholass-MacBook-Pro.local>
2023-04-07 17:42:58 -1000

    added caching

--- src/components/SearchComponent/index.js
+++ src/components/SearchComponent/index.js
@@ -1,34 +1,43 @@
-import './styles.css'
-import { useState } from 'react';
-import CardComponent from '../CardComponent';
-import CarouselComponent from '../CarouselComponent';
+import "./styles.css";
+import { useState } from "react";
+import CardComponent from "../CardComponent";
+import CarouselComponent from "../CarouselComponent";
 
-function SearchComponent(){ 
-    const searchSong = (searchTerm) => {
-        fetch('https://itunes.apple.com/search?term=' + searchTerm + '&media=music')
-        .then(response => response.json())
-        .then(data => setResults(data.results));
+function SearchComponent() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  console.log(results);
+
+  const searchSong = (searchTerm) => {
+    if (localStorage.getItem(searchTerm)) {
+      setResults(JSON.parse(localStorage.getItem(searchTerm)).results)
+    } else {
+      fetch("https://itunes.apple.com/search?term=" + searchTerm + "&media=music")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => 
+      {
+        localStorage.setItem(searchTerm, JSON.stringify(data));
+
+        setResults(data.results)
+      });
     }
-    
-const [searchTerm, setSearchTerm] = useState('');
-const [results, setResults] = useState([]);
-console.log(results);
+  
+  };
 
   return (
     <div className="search-component">
-      <input className="search" placeholder='Twista' onChange={(e) => setSearchTerm(e.target.value)}/>
+      <input
+        className="search"
+        placeholder="Twista"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
       <button onClick={() => searchSong(searchTerm)}>Search</button>
 
-      <CarouselComponent results={results}/>
-
-
-
-
-      
- 
+      <CarouselComponent results={results} />
     </div>
   );
-    }
-
+}
 
 export default SearchComponent;
